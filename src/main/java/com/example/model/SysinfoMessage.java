5855878892cd4de2d959a270b492a85e3671a90c
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-01-22 16:04:33 +0000

    refactor: getBytes method in SysinfoMessage

--- src/main/java/com/example/model/SysinfoMessage.java
+++ src/main/java/com/example/model/SysinfoMessage.java
@@ -1,10 +1,14 @@
 package com.example.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Data
@@ -17,4 +21,10 @@ public class SysinfoMessage {
     private long readTime;
     @JsonProperty("metrics")
     private Map<String, Double> metrics;
+
+    @JsonIgnore
+    public byte[] getBytes() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(this).getBytes(StandardCharsets.UTF_8);
+    }
 }
